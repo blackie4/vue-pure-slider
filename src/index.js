ffe675afacf7f6e7875fa4840f0e72e42b2578c5
mingyi.ymy <mingyi.ymy@alibaba-inc.com>
2017-08-10 11:34:13 +0800

    feat: Support script  usage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import vuePureSlider from './vue-pure-slider.vue';
 
+function install(Vue) {
+  Vue.component('vuePureSlider', vuePureSlider);
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(install);
+}
+
 export default vuePureSlider;
--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-  	index: './src/index.js',
-  	example: './example/index.js',
+  	index: './src/index.js'
   },
   output: {
     filename: '[name].js',
     libraryTarget: "umd",
     path: path.resolve(__dirname, 'dist')
   },
-   plugins: [
-    new HtmlWebpackPlugin({
-      filename: './index.html',
-      template: './example/index.html',
-      chunks: ['example'],
-      hash: true
-    })
-  ],
   resolve: {
     extensions: ['.js', '.vue', '.less']
   },
